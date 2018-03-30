6e28a75994a63111e796c9c1d54826eef015f28a
Dan Engel <dengel29@gmail.com>
2018-03-30 17:29:13 +0800

    signature created! great success

--- app/controllers/api/v1/signatures_controller.rb
+++ app/controllers/api/v1/signatures_controller.rb
@@ -16,9 +16,6 @@ class Api::V1::SignaturesController < Api::V1::BaseController
   require 'digest/md5'
   require 'net/http'
 
-  @app_secret = "d80b3b3da95604eccb78be543c7edf56"
-  @app_key = "dada1ae4c46105bf672"
-
 
   def receive_params
     respond_to :json
@@ -27,14 +24,12 @@ class Api::V1::SignaturesController < Api::V1::BaseController
 
     @signature = Signature.new(signature_params)
 
-    puts "This is the data: "
-
-
     timestamp = @signature.timestamp
     # puts timestamp
 
     v = @signature.v
     # puts v
+    format = @signature.format
 
     source_id = @signature.source_id
     # puts source_id
@@ -42,14 +37,12 @@ class Api::V1::SignaturesController < Api::V1::BaseController
     shop_no = @signature.shop_no
     # puts shop_no
 
-    signature = @signature.signature
     # puts signature
 
     order_id = @signature.order_id
-    puts "Below is body: "
 
     body = @signature.body
-    puts body
+
     puts "Everything is okay"
 
 
@@ -59,30 +52,35 @@ class Api::V1::SignaturesController < Api::V1::BaseController
       timestamp: timestamp,
       v: v,
       source_id: source_id,
-      shop_no: shop_no,
-      signature: signature,
-      body: body
+      body: body,
+      format: format
     }
+    puts ""
     puts @params
+    puts ""
 
     assemble_signature(@params)
   end
 
   # params is a hash, with all the signature attributes
   def get_order_id(params)
-    puts "Initiate Get Order id"
-    puts params.class
-
-    order_id = params.fetch(:body)
-    puts order_id.class
-    puts "Order ID is a: "
-    puts order_id
+    puts "Initiate Get Order id..."
+    puts ""
+    order_id = params[:body]#['order_id']#.fetch(:body)
+    begin
+      order_id = eval(order_id)
+    rescue Exception => ex
+      print ex
+    end
+    #puts order_id['order_id']
     return order_id
   end
 
   # get_order_id(@params)
 
   def signaturize(params)
+      puts "Initiate signaturize..."
+      puts ""
       # Sort by key, alphabetical
       params = Hash[params.sort_by { |key, value| key }]
       # Deletes Body from params
@@ -95,14 +93,25 @@ class Api::V1::SignaturesController < Api::V1::BaseController
   end
 
   def assemble_signature(params)
-    puts "Initiate signature assembly"
+    app_secret = "d80b3b3da95604eccb78be543c7edf56"
+    app_key = "dada1ae4c46105bf672"
+    puts "Initiate signature assembly..."
+    puts ""
     sig = signaturize(params)
     order_id = get_order_id(params)
-    final_sig = @app_secret + @app_key + order_id + sig + @app_secret
+    middle_sig = "body#{order_id}#{sig}"
+    puts "the middle of the sig"
+    puts middle_sig
+    puts ""
+    final_sig = app_secret + app_key + middle_sig + app_secret
+
     puts "Here's the final sig: "
     puts final_sig
+    puts ""
+
     md5_sig = Digest::MD5.hexdigest(final_sig)
-    puts md5_sig
+    puts "Here's the upcased md5_sig"
+    puts md5_sig.upcase
     return md5_sig
   end
 
@@ -110,8 +119,7 @@ class Api::V1::SignaturesController < Api::V1::BaseController
 
   def signature_params
     # These params are required to create an event in event/new view
-    params.delete(:format)
-    params.permit(:timestamp, :v, :source_id, :shop_no, :signature, :order_id, {:body => [:order_id]})
+    params.permit(:timestamp, :v, :source_id, :shop_no, :signature, :order_id, :format, {:body => [:order_id]})
   end
 end
 
